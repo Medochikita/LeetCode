3066cb5ca6665d8b985cc1276ea0615f869b2a1f
Medochikita <kutil.ondra@outlook.cz>
2024-09-22 17:56:28 +0200

    nkn

--- imageSmooth.py
+++ imageSmooth.py
@@ -2,6 +2,11 @@
 An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight 
 surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, 
 we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+(img[i-1][k-1] + img[i-1][k] + img[i-1][k+1]
++ img[i][k-1] + img[i][k] + img[i][k+1]
++ img[i+1][k-1] + img[i+1][k] + img[i+1][k+1])/9
+
 '''
 
 # TODO Not finnished, this only does the job if all 8 surrounding cells are present
@@ -11,13 +16,41 @@ import math
 
 class Solution:
     def imageSmoother(img: list) -> list:
-        for i in range(1, len(img)-1):
-            for k in range(1, len(img[i])-1):
-                avg = (img[i-1][k-1] + img[i-1][k] + img[i-1][k+1]
-                       + img[i][k-1] + img[i][k] + img[i][k+1]
-                       + img[i+1][k-1] + img[i+1][k] + img[i+1][k+1])/9
+        for i in range(0, len(img)):
+            for k in range(0, len(img[i])):
+                s = 0
+                counter = 0
+                if img[i-1][k-1]:
+                    s += img[i-1][k-1]
+                    counter += 1
+                if img[i-1][k]:
+                    s += img[i-1][k]
+                    counter += 1
+                if img[i-1][k+1]:
+                    s += img[i-1][k+1]
+                    counter += 1
+                if img[i][k-1]:
+                    s += img[i][k-1]
+                    counter += 1
+                s += img[i][k]
+                counter += 1
+                if img[i][k+1]:
+                    s += img[i][k+1]
+                    counter += 1
+                if img[i+1][k-1]:
+                    s += img[i+1][k-1]
+                    counter += 1
+                if img[i+1][k]:
+                    s += img[i+1][k]
+                    counter += 1
+                if img[i+1][k+1]:
+                    s += img[i+1][k+1]
+                    counter += 1
                 
-                img[i][k] = math.floor(avg)
+
+
+                
+                img[i][k] = math.floor(s/counter)
 
 
         return img
